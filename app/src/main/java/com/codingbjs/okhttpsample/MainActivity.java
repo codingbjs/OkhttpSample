6cbbb5759c167bcca46c493eb431d0c37919653d
CDBJS <codingbjs@gmail.com>
2022-02-14 18:31:41 +0900

    OkhttpClient Test method 추가

--- app/src/main/java/com/codingbjs/okhttpsample/MainActivity.java
+++ app/src/main/java/com/codingbjs/okhttpsample/MainActivity.java
@@ -1,14 +1,51 @@
 package com.codingbjs.okhttpsample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getUrlPage("https://www.naver.com");
+    }
+
+
+    private void getUrlPage (String url) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "onFailure() called with: call = [" + call + "], e = [" + e + "]");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                Log.d(TAG, "onResponse() called with: call = [" + call + "], response = [" + response + "]");
+                Log.d(TAG, "onResponse: " + Objects.requireNonNull(response.body()).string());
+            }
+        });
     }
 }
